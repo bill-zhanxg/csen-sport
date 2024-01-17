868dbca0c939a2b775ca27e8e46a63989465c868
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-01-17 20:17:32 +1100

    fixing some texts

--- app/weekly-sport/components/ImportPage.tsx
+++ app/weekly-sport/components/ImportPage.tsx
@@ -332,13 +332,17 @@ export function ImportPage() {
 					<li className={`step ${step >= 3 ? 'step-primary' : ''}`}>Check & Edit</li>
 					<li className={`step ${step >= 4 ? 'step-primary' : ''}`}>Finish</li>
 				</ul>
-				<h1 className="text-4xl font-bold">Import Weekly Sport PDF to Database</h1>
+				<h1 className="text-4xl text-center font-bold">Import Weekly Sport PDF to Database</h1>
 
 				{step === 1 && (
 					<>
-						<p className="text-xl font-bold max-w-xl">
+						<p className='text-xl font-bold text-error text-center max-w-xl'>
+							Before you begin, make sure all teachers are registered and being given the <span className='text-info'>teacher</span> role for linking
+							with each game in <Link href='/users' className='link link-primary'>the User page</Link>
+						</p>
+						<p className="text-xl font-bold text-center max-w-xl">
 							Please find the latest PDF from{' '}
-							<Link className="link-secondary link-hover" href="https://csen.org.au/semester-sport/" target="_blank">
+							<Link className="link-secondary link" href="https://csen.org.au/semester-sport/" target="_blank">
 								CSEN
 							</Link>{' '}
 							and import it here
