150567d9e3577dfb5bc17f4ed77d20f8da648667
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-04-20 13:13:12 +1000

    chore(ticket): center text

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,3 +1,7 @@
 export default async function Tickets() {
-	return <div className="text-3xl">Work in progress</div>;
+	return (
+		<div className="flex justify-center items-center w-full h-full">
+			<div className="text-3xl">Work in progress</div>
+		</div>
+	);
 }
