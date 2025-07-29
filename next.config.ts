80a8cb1b9c4699a1af46c9052b4222db24208c3b
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2025-07-29 18:08:10 +1000

    feat: rewrite next.config.ts to include updated Sentry configuration and experimental features fix: fixed image upload size uncaught error increase size to 30mb

---
+++ next.config.ts
@@ -0,0 +1,67 @@
+import { withSentryConfig } from '@sentry/nextjs';
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'icotar.com',
+			},
+			{
+				protocol: 'https',
+				hostname: '*.xata.sh',
+			},
+		],
+	},
+	webpack: (config, { isServer }) => {
+		config.resolve.alias.canvas = false;
+		if (isServer) {
+			config.ignoreWarnings = [{ module: /opentelemetry/ }];
+		}
+		return config;
+	},
+	experimental: {
+		authInterrupts: true,
+		ppr: true,
+		reactCompiler: true,
+		typedRoutes: true,
+		typedEnv: true,
+		useCache: true,
+		clientSegmentCache: true,
+		// Waiting for Sentry to support dynamicIO: https://github.com/getsentry/sentry-javascript/issues/14118
+		// dynamicIO: true,
+
+		serverActions: {
+			bodySizeLimit: '30mb',
+		},
+	},
+};
+
+export default withSentryConfig(nextConfig, {
+	// For all available options, see:
+	// https://github.com/getsentry/sentry-webpack-plugin#options
+
+	org: 'billzhanxg',
+	project: 'csen-sport',
+
+	// Only print logs for uploading source maps in CI
+	silent: !process.env.CI,
+
+	// For all available options, see:
+	// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+	// Upload a larger set of source maps for prettier stack traces (increases build time)
+	widenClientFileUpload: true,
+
+	// Automatically annotate React components to show their full name in breadcrumbs and session replay
+	reactComponentAnnotation: {
+		enabled: true,
+	},
+
+	// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+	tunnelRoute: '/monitoring',
+
+	// Automatically tree-shake Sentry logger statements to reduce bundle size
+	disableLogger: true,
+});
