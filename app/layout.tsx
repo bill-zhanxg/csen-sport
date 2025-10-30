ad678475f1000da2e9c5f2ed5e9fe2f0d9bafdee
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2025-10-30 19:42:16 +1100

    feat: implement loading state with GenericLoading component and refactor MainLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import { headers } from 'next/headers';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Suspense } from 'react';
 import 'server-only';
 import { Toaster } from 'sonner';
 import { authC } from './cache';
@@ -13,6 +14,7 @@ import { FeedbackDialog } from './components/Feedback';
 import { HandleUserTimezone } from './components/HandleUserTimezone';
 import { NavBar } from './components/NavBar';
 import { SentrySetUser } from './components/SentrySetUser';
+import { GenericLoading } from './globalComponents/GenericLoading';
 
 import './globals.css';
 
@@ -29,40 +31,46 @@ export const metadata: Metadata = {
 const xata = getXataClient();
 
 export default async function MainLayout({ children }: { children: React.ReactNode }) {
-	const session = await authC();
-	const ip = (await headers()).get('x-forwarded-for')?.split(':')[0];
-
 	return (
 		<html lang="en">
 			<body>
-				{session ? (
-					isBlocked(session) ? (
-						<>
-							<h1 className="text-2xl">
-								Sorry but you&apos;ve been blocked from accessing this site.{' '}
-								<Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="link link-primary" target="_blank">
-									Click here
-								</Link>
-							</h1>
-							<Image src={SadCat} alt="Sad" width={1000} height={1000} className="w-52 h-52" />
-						</>
-					) : (
-						<>
-							<NavBar session={session}>{children}</NavBar>
-							<FeedbackDialog session={session} />
-							{session.user.auto_timezone && <HandleUserTimezone />}
-							<SentrySetUser user={{ ...session.user, ip_address: ip }} />
-							{/* TODO: Add this back after react joyride is fixed */}
-							{/* {!session.user.guided && <ReactJoyride />} */}
-						</>
-					)
-				) : (
-					children
-				)}
+				<Suspense fallback={<GenericLoading />}>
+					<Content>{children}</Content>
+				</Suspense>
 
 				<BarOfProgress />
 				<Toaster richColors closeButton />
 			</body>
 		</html>
+	);
+}
+
+async function Content({ children }: { children: React.ReactNode }) {
+	const session = await authC();
+	const ip = (await headers()).get('x-forwarded-for')?.split(':')[0];
+
+	return session ? (
+		isBlocked(session) ? (
+			<>
+				<h1 className="text-2xl">
+					Sorry but you&apos;ve been blocked from accessing this site.{' '}
+					<Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="link link-primary" target="_blank">
+						Click here
+					</Link>
+				</h1>
+				<Image src={SadCat} alt="Sad" width={1000} height={1000} className="w-52 h-52" />
+			</>
+		) : (
+			<>
+				<NavBar session={session}>{children}</NavBar>
+				<FeedbackDialog session={session} />
+				{session.user.auto_timezone && <HandleUserTimezone />}
+				<SentrySetUser user={{ ...session.user, ip_address: ip }} />
+				{/* TODO: Add this back after react joyride is fixed */}
+				{/* {!session.user.guided && <ReactJoyride />} */}
+			</>
+		)
+	) : (
+		children
 	);
 }
