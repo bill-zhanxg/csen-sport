c29ea0709e20f8d90b20ffa25041c7095c516141
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2025-10-30 20:37:47 +1100

    feat: refactor vitest configuration to use dynamic import for React plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,30 +1,33 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [react()],
-	test: {
-		exclude: [
-			'**/node_modules/**',
-			'**/dist/**',
-			'**/cypress/**',
-			'**/.{idea,git,cache,output,temp}/**',
-			'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+export default defineConfig(async () => {
+	const react = await import('@vitejs/plugin-react').then((m) => m.default);
+	
+	return {
+		plugins: [react()],
+		test: {
+			exclude: [
+				'**/node_modules/**',
+				'**/dist/**',
+				'**/cypress/**',
+				'**/.{idea,git,cache,output,temp}/**',
+				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
 
-			// Exclude playwright tests
-			'tests/**',
-		],
-		alias: {
-			'@/': new URL('./', import.meta.url).pathname,
+				// Exclude playwright tests
+				'tests/**',
+			],
+			alias: {
+				'@/': new URL('./', import.meta.url).pathname,
+			},
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: 'vitest.setup.ts',
+
+			// browser: {
+			// 	provider: 'playwright',
+			// 	enabled: !process.env.CI,
+			// 	name: 'chromium',
+			// },
 		},
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: 'vitest.setup.ts',
-
-		// browser: {
-		// 	provider: 'playwright',
-		// 	enabled: !process.env.CI,
-		// 	name: 'chromium',
-		// },
-	},
+	};
 });
